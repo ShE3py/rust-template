2442e4ccb1f8a0e1680c9686f6829c64073b26c5
Lieselotte <52315535+she3py@users.noreply.github.com>
2025-11-07 19:10:18 +0100

    Update to rustc 1.93.0-nightly

--- check-lints.sh
+++ check-lints.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-clippy-driver +nightly -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | sort | diff - default-lint-levels.txt
+set -ex
+
+diff <(sort lint-levels.txt) lint-levels.txt
+diff <(clippy-driver +nightly -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | sort) default-lint-levels.txt
+diff <(wc -l < lint-levels.txt) <(wc -l < default-lint-levels.txt)
