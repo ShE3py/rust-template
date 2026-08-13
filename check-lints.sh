e5423840beb27398f78b1f55bf43b8ed500d5eed
Lieselotte <52315535+she3py@users.noreply.github.com>
2026-08-13 21:11:35 +0200

    Update to rustc 1.97.1

--- check-lints.sh
+++ check-lints.sh
@@ -9,6 +9,7 @@ diff <(wc -l < lint-levels.txt) <(wc -l < default-lint-levels.txt)
 clippy-driver +nightly -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | sort > default-lint-levels.stdout
 cd tools
 cargo run --bin relint
+mv ../default-lint-levels.{stdout,txt}
 
 cat > ../template/.cargo/config.toml << EOF
 [build]
@@ -17,6 +18,3 @@ EOF
 echo
 cargo run --bin reflag | sed 's/^/    /' | sed 's/ $//' >> ../template/.cargo/config.toml
 echo ']' >> ../template/.cargo/config.toml
-
-cd ..
-mv default-lint-levels.stdout default-lint-levels.txt
