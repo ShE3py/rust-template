ce0ffdd1fd4a5c6acdf49c81a7477f29b79371fe
Lieselotte <52315535+she3py@users.noreply.github.com>
2026-09-04 13:53:40 +0200

    feat: update to rustc 1.98.1

--- check-lints.sh
+++ check-lints.sh
@@ -7,7 +7,7 @@ diff <(wc -l < lint-levels.txt) <(wc -l < default-lint-levels.txt)
 
 # Update lint store
 rustc +nightly -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | grep -v rustc | sort > default-lint-levels.stdout
-rustdoc +stage1 -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | grep rustdoc | sort >> default-lint-levels.stdout
+rustdoc +nightly -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | grep rustdoc | sort >> default-lint-levels.stdout
 clippy-driver +nightly -Z unstable-options --print=crate-root-lint-levels --edition=2024 /dev/null | grep clippy | sort >> default-lint-levels.stdout
 cd tools
 cargo run --bin relint
@@ -20,3 +20,4 @@ EOF
 echo
 cargo run --bin reflag | sed 's/^/    /' | sed 's/ $//' >> ../template/.cargo/config.toml
 echo ']' >> ../template/.cargo/config.toml
+rm -r ./-
